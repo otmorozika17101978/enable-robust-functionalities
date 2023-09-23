6535b1e74cf11025ed71ebd152876f0ca756524e
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-23 05:19:34 +0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,33 +1,46 @@
-main();
-    data.forEach(item => {
-}
-        console.log("Random Number: " + item);
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    for item in data:
+def generate_random_data():
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
 
 
+
+
+def main():
+if __name__ == "__main__":
+    main()
+import random    });
         data.push(Math.floor(Math.random() * 100) + 1);
     return data;
-    const data = [];
     }
-    for (let i = 0; i < 10; i++) {
-function main() {
 
-    });
+        console.log("Random Number: " + item);
+
 function generateRandomData() {
+}
+    data.forEach(item => {
+main();
     const data = generateRandomData();
-}    const data = generateRandomData();
-
-        console.log("Random Number: " + item);
+}
     for (let i = 0; i < 10; i++) {
 
 function main() {
-}
-    });
+    const data = [];    const data = generateRandomData();
 
-    data.forEach(item => {
-    const data = [];
-    return data;
-        data.push(Math.floor(Math.random() * 100) + 1);
-}
 main();
 function generateRandomData() {
+    data.forEach(item => {
     }
+        console.log("Random Number: " + item);
+    return data;
+
+}
+
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+    });
+function main() {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
